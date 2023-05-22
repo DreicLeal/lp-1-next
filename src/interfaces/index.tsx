f90748272b84c41da9c93aa12398d2a8c55b5dba
unknown <dreicleal@hotmail.com>
2023-05-22 15:09:08 -0300

    our team section

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -12,9 +12,16 @@ export interface IService {
     name: string;
     description: string;
   }
+export interface IPeople {
+    id: number;
+    name: string;
+    role: string;
+    image: StaticImageData;
+  }
   
   export interface IUser {
     name: string;
     role: string;
     insights: IInsights[]
+    people: IPeople[]
   }
